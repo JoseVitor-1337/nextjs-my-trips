384f29aadb9bcfb7e39c75d0fd866c3b9fce2e73
JoseVitor-1337 <josevitordemelo@yahoo.com>
2021-12-11 20:53:39 -0300

    .

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+
+export default function Home() {
+	const teste = ""
+
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Página Home</title>
+			</Head>
+
+			<main className={styles.main}></main>
+		</div>
+	)
+}
