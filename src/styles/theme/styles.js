2d2a510870e2fb45468568e98a9e1561154f6505
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-02-25 14:38:24 -0300

    Fix: removendo lixo do projeto.

--- src/styles/theme/styles.js
+++ src/styles/theme/styles.js
@@ -6,28 +6,11 @@ const global = {
 			fontSize: "90%"
 		},
 
-		/*
-    input: {
-      "&:focus": {
-        borderColor: `var(--chakra-colors-green-500) !important`,
-        boxShadow: `0 0 0 1px var(--chakra-colors-green-500) !important`
-      },
-    },
-    */
-
 		"@media only screen and (max-width: 600px)": {
 			"html, body": {
 				fontSize: "85%"
 			}
 		},
-
-		".react-pdf__Page__canvas": {
-			mb: "4"
-		},
-
-		nav: {
-			backgroundColor: mode("whiteAlpha.900", "gray.800")(props)
-		}
 	})
 }
 
